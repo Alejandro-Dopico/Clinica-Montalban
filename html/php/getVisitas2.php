bbadd3465e31a38c62bab477c9ad52ed728aedde
sjawara <113584527+sjawara@users.noreply.github.com>
2024-01-19 20:17:42 +0100

    Add files via upload

---
+++ html/php/getVisitas2.php
@@ -0,0 +1,50 @@
+
+<?php
+// Tu código PHP aquí
+session_start();
+include 'conexion_be.php';
+
+//$query = "SELECT * FROM cita WHERE fecha > CURDATE() OR (fecha = CURDATE() AND hora > CURTIME())"; 
+$query = "SELECT idTrabajador,fecha, HOUR(hora) AS hora  FROM cita WHERE fecha > CURDATE() OR (fecha = CURDATE() AND HOUR(hora) > HOUR(CURTIME()))";
+$result = mysqli_query($conexion, $query);
+$contenido = array();
+$i = 0;
+
+if ($result) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $visita = array(
+            "dia" => $row['fecha'],
+            "hora" => $row['hora'],
+            "trabajador" => $row['idTrabajador']
+        );
+        $i++;
+        $contenido[$i] = $visita;
+    }
+
+    
+}
+$contenidoAñadir = json_encode($contenido);
+$file = '../assets/JSON/visitas.json';
+file_put_contents($file, $contenidoAñadir);
+/*
+if (file_put_contents($file, $contenidoAñadir) !== false) {
+    echo "Archivo JSON actualizado correctamente.";
+} else {
+    echo "Hubo un error al intentar escribir en el archivo JSON.";
+}
+*/
+
+
+$contenidoJSON = file_get_contents($file);
+
+echo $contenidoJSON;
+
+/*
+// Mostrar el contenido de $contenido usando la etiqueta <pre>
+echo "<pre>";
+print_r($contenidoJSON);
+echo "</pre>";
+*/
+?>
+
+
---
+++ html/php/insertCitas.php
@@ -0,0 +1,44 @@
+<?php
+    include 'conexion_be.php';
+    
+    session_start();
+    
+    if (!isset($_SESSION['usuario'])) {
+        header("Location: ../login.php");
+        exit;
+    }
+    
+    $idTrabajador = $_POST['idTrabajador'];
+    $idCliente = $_POST['idCliente'];
+    $fecha = $_POST['fecha'];
+    $hora = $_POST['hora'];
+    $DNI_USER = $_SESSION['usuario'];
+
+    // Utilizar consultas preparadas para evitar problemas de SQL injection
+    $query = "INSERT INTO cita (idTrabajador, idCliente, fecha, hora) VALUES (?, (SELECT idCliente FROM cliente WHERE DNI=?), ?, ?)";
+
+    // Preparar la consulta
+    $stmt = mysqli_prepare($conexion, $query);
+
+    // Vincular los parámetros
+    mysqli_stmt_bind_param($stmt, "isss", $idTrabajador, $DNI_USER, $fecha, $hora);
+
+    // Ejecutar la consulta
+    $result = mysqli_stmt_execute($stmt);
+
+    $response = array();
+
+    if($result){
+        $response['success'] = true;
+        $response['message'] = 'Cita insertada con éxito';
+    } else {
+        $response['success'] = false;
+        $response['message'] = 'Error al insertar cita: ' . mysqli_error($conexion);
+    }
+
+    // Cerrar la consulta preparada
+    mysqli_stmt_close($stmt);
+
+    header('Content-Type: application/json');
+    echo json_encode($response);
+?>

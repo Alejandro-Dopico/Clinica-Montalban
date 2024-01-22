e6f766dde4e86056557f0f1247f0b4368d1bf49d
Alejandro-Dopico <114652793+Alejandro-Dopico@users.noreply.github.com>
2024-01-22 10:32:53 +0100

    insertCitas.php

--- html/php/insertCitas.php
+++ html/php/insertCitas.php
@@ -1,4 +1,7 @@
 <?php
+
+    /*  Basicamente esta funcion recoge todo lo del formulario de calendario
+        y hace un insert a la base de datos con una cita nueva. */
     include 'conexion_be.php';
     
     session_start();
@@ -12,16 +15,17 @@
     $idCliente = $_POST['idCliente'];
     $fecha = $_POST['fecha'];
     $hora = $_POST['hora'];
+    $descripcion = $_POST['descripcion'];
     $DNI_USER = $_SESSION['usuario'];
 
     // Utilizar consultas preparadas para evitar problemas de SQL injection
-    $query = "INSERT INTO cita (idTrabajador, idCliente, fecha, hora) VALUES (?, (SELECT idCliente FROM cliente WHERE DNI=?), ?, ?)";
+    $query = "INSERT INTO cita (idTrabajador, idCliente, fecha, hora, descripcion) VALUES (?, (SELECT idCliente FROM cliente WHERE DNI=?), ?, ?, ?)";
 
     // Preparar la consulta
     $stmt = mysqli_prepare($conexion, $query);
 
     // Vincular los parámetros
-    mysqli_stmt_bind_param($stmt, "isss", $idTrabajador, $DNI_USER, $fecha, $hora);
+    mysqli_stmt_bind_param($stmt, "issss", $idTrabajador, $DNI_USER, $fecha, $hora, $descripcion);
 
     // Ejecutar la consulta
     $result = mysqli_stmt_execute($stmt);
